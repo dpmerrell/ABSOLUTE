a695c950a5c3321a55dc21b3ed8685d8e071e8e3
David Merrell <merrell@wm1ee-a2a.broadinstitute.org>
2023-11-14 14:45:11 -0500

    python package script interface

--- pyabsolute/run_absolute.py
+++ pyabsolute/run_absolute.py
@@ -10,5 +10,16 @@ def run_absolute():
 
 if __name__=="__main__":
 
+    #####################################
+    # Run ABSOLUTE as a script
+    #####################################
+
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("relative_cn_file", help="Relative copy number file")
+    parser.add_argument("out_file", help="Output file")
+    parser.add_argument("--point_mutation_maf", help="MAF containing point mutations (optional)")
+
     run_absolute()
 
